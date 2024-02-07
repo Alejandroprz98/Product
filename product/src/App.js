8e5fab80a0b15c4ea52321d5cc078540f4f2024e
Alejandroprz98 <alejandroperezrosas@hotmail.com>
2024-02-07 17:51:42 -0600

    Se agrega el Proyecto

---
+++ product/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    
+    <div className="App">
+      <header className="App-header">
+        
+<div class="sidenav">
+  <a href="#">Administración</a>
+  <a href="#">Foro</a>
+  <a href="#">App</a>
+</div>
+
+<div class="content">
+  <h2>Residencial Los Robles</h2>
+  <p>No hay mejor lugar para formar tu familia que aquí.</p>
+</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
